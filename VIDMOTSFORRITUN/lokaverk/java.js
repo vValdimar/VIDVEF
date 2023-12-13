0ada14318c972a6224c00725b23dd646c8711e4e
vValdimar <valdimarkr21@tskoli.is>
2023-12-13 21:07:55 +0000

    its joever 🗿

--- VIDMOTSFORRITUN/lokaverk/java.js
+++ VIDMOTSFORRITUN/lokaverk/java.js
@@ -49,12 +49,62 @@ function adjustLongitude(longitude) {
 
 function setColors(sunHeight) {
   sunHeight = (1-sunHeight/200)*200;
-  document.body.style.transition = 'background 1s ease'
-  document.body.style.background = `linear-gradient(to bottom, 
+  document.getElementById('weatherstuff').style.opacity = `${sunHeight}%`
+  document.body.style.background = `linear-gradient(to bottom,
   rgb(${sunHeight/1.5}, ${sunHeight/1.25}, ${sunHeight*1.25}), 
   rgb(${10+sunHeight*3}, ${10+sunHeight*2.5}, ${50+sunHeight*1.5}))`;
   document.getElementById('sun').setAttribute('cy', `${120-sunHeight/2}%`)
   document.getElementById('ground').setAttribute('fill', `rgb(${20+sunHeight/2}, ${20+sunHeight}, ${20+sunHeight/2})`)
+}
+
+const cloudsLeft = document.getElementById('left');
+const cloudsRight = document.getElementById('right');
+
+function moveClouds(onScreen) {
+
+  [cloudsLeft, cloudsRight].forEach(side => {
+    const clouds = side.getElementsByTagName('ellipse');
+    const offScreenX = side.getAttribute('data-offscreen-x');
+
+    for (const cloud of clouds) {
+      const cloudX = cloud.getAttribute('data-cx');
+
+      if (onScreen) {
+        cloud.setAttribute('cx', cloudX);
+      } else {
+        cloud.setAttribute('cx', offScreenX);
+      }
+    }
+  })
+}
+
+let isSnow = false;
+let raining = false;
+const rainbox = document.getElementById("rainbox");
+
+function createRaindrop() {
+  if (!raining) { return; }
+  
+  const raindrop = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  raindrop.setAttribute("class", (isSnow ? "snowflake": "raindrop"));
+  raindrop.setAttribute("x1", `${Math.random() *100}vw`);
+  raindrop.setAttribute("y1", 0);
+  raindrop.setAttribute("x2", raindrop.getAttribute("x1"));
+  raindrop.setAttribute("y2", (isSnow ? 10: 40));
+  rainbox.appendChild(raindrop);
+
+  const animation = raindrop.animate(
+    [
+      { transform: "translateY(0)" },
+      { transform: `translateY(100vh)` }
+    ],
+    {
+      duration: Math.random() * 500 + (isSnow ? 500 : 100)
+    }
+  );
+  animation.addEventListener('finish', () => {
+    raindrop.remove();  
+  })
 }
 
 function displayCountry() {
@@ -78,8 +128,6 @@ function displayCountry() {
 
 function toggleMap() {
   const mapContainer = document.getElementById('map-container');
-  const stuff = document.getElementById('stuff');
-  const button = document.getElementById('icon');
 
   isMapVisible = !isMapVisible;
 
@@ -107,16 +155,31 @@ function displayWeatherResults() {
     .then(data => {
         const unixTime = data.dt + data.timezone;
         const date = new Date(unixTime * 1000);
+        var bbox = arrow.getBBox();
+        const centerX = bbox.x + bbox.width * 0.51;
+        const centerY = bbox.y + bbox.height * 0.53;
+        const weatherMain = data.weather[0].main;
         document.getElementById("city").innerText = data.name;
         document.getElementById("time").innerText = date.toLocaleTimeString('en-US');
         document.getElementById("temperature").innerText = `${(data.main.temp-273.15).toFixed(1)}°C`
-        document.getElementById("weather").innerText = data.weather[0].main;
+        document.getElementById("weather").innerText = weatherMain;
+        document.getElementById("weathericon").setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+        document.getElementById("arrow").setAttribute('transform', `rotate(${data.wind.deg-90} ${centerX} ${centerY})`);
+        document.getElementById("wind").innerText = `${data.wind.speed}m/s`;
+
 
         const midDay = (data.sys.sunrise + data.sys.sunset)/2;
         const sunLevel = (Math.abs(data.dt - midDay));
         setColors(Math.round(sunLevel/43200*200));
+        moveClouds(["Clouds", "Rain", "Snow"].includes(weatherMain));
+        rainbox.innerHTML = ''
+        isSnow = weatherMain == "Snow";
+        raining = ["Snow", "Rain"].includes(weatherMain);
+        console.log(isSnow, weatherMain);
+        
     })
     .catch(error => {
         console.log('Error...', error);
     })
 }
+setInterval(createRaindrop, (isSnow ? 40: 10));
